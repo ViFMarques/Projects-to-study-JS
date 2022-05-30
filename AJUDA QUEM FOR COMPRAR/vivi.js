75125874e0aed908c4c82c057c9a20fd3caa8471
Vitória Marques <vivi.ferrmarques@gmail.com>
2022-05-30 13:00:08 -0300

    o amigo do consumidor
    adicionado JavaScript, melhorado o CSS e a página agora tem responsividade.

---
+++ AJUDA QUEM FOR COMPRAR/vivi.js
@@ -0,0 +1,17 @@
+var yesButton = document.getElementsByClassName("yesbutton")
+var noButton = document.getElementsByClassName("nobutton")
+
+yesButton.addEventListener('click', changeColor())
+
+function changeColor(){
+    yesButton.innerHTML = "ok"
+}
+
+function activateCalculator(){
+    if(document.getElementById('yesbutton').checked && document.getElementById('yesbutton1').checked && document.getElementById('yesbutton2').checked && document.getElementById('yesbutton3').checked && document.getElementById('yesbutton4').checked){
+        document.getElementById("answer").innerHTML = "Você pode comprar! &#129321;";
+    } else {
+        document.getElementById("answer").innerHTML = "Você NÃO pode comprar! &#128555;";
+    }
+}
+
